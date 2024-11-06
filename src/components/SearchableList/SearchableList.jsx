4350dc2332e8294e8fab633042334aa1d9588740
sudarshan.shinde <sudarshan.shinde@inadev.com>
2024-11-06 22:56:26 +0530

    debouncing and render props

---
+++ src/components/SearchableList/SearchableList.jsx
@@ -0,0 +1,31 @@
+import React, { useRef, useState } from "react";
+
+const SearchableList = ({ items, children, itemKeyFn }) => {
+  const lastChange = useRef();
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchResults = items.filter((item) =>
+    JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  const handdleChange = (e) => {
+    if (lastChange.current) {
+      clearTimeout(lastChange.current);
+    }
+
+    lastChange.current = setTimeout(() => {
+      lastChange.current = null;
+      setSearchTerm(e.target.value);
+    }, 500);
+  };
+  return (
+    <div className="searchable-list">
+      <input type="search" placeholder="search" onChange={handdleChange} />
+      <ul>
+        {searchResults.map((item) => (
+          <li key={itemKeyFn(item)}>{children(item)}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SearchableList;
